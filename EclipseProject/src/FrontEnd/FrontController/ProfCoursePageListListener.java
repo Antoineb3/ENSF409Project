a3be04eaa7139f9cb831bccb1b6f3f605baf4c23
Ross Bartlett <rossjbartlett@gmail.com>
2018-04-05 16:50:56 -0600

    deleted old listListeners, renamed CLICK_listeners

--- EclipseProject/src/FrontEnd/FrontController/ProfCoursePageListListener.java
+++ EclipseProject/src/FrontEnd/FrontController/ProfCoursePageListListener.java
@@ -1,53 +1,49 @@
-
+/**
+ * 
+ */
 package FrontEnd.FrontController;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JList;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
-
-import FrontEnd.View.ProfCoursePage;
 import FrontEnd.View.ProfGUI;
 import SharedObjects.Assignment;
 
 /**
- * The ListListener of the assingmentList on the ProfCoursePage to select an assignment to view 
+ * Listener for the assignmentList on the ProfCoursePage using MouseAdapter instead of ListSelectionListener
  */
-public class ProfCoursePageListListener implements ListSelectionListener {
-	/**
-	 * The panel with the button calling this listener
-	 */
-	private ProfCoursePage panel;
+public class ProfCoursePageListListener extends MouseAdapter{
 	/**
 	 * the controller constructing this listener
 	 */
 	private ProfController controller;
 
-	public ProfCoursePageListListener(ProfCoursePage p, ProfController c) {
-		panel = p;
+	public ProfCoursePageListListener(ProfController c) {
 		controller=c;
-		
+
 	}
 
 	/**
-	 * Updates the selected assignmentPage's assignment, refreshes the assignment page, switches to it
+	 * On double click, Updates the selected assignmentPage's assignment, refreshes the assignment page, switches to it
 	 */
 	@Override
-	public void valueChanged(ListSelectionEvent e) {
-		//get the selected course
-		JList<Assignment> list = panel.getAssignmentList();
-		int index = list.getSelectedIndex();
+	public void mouseClicked(MouseEvent evt) {
+
+		JList<Assignment> list = (JList<Assignment>) evt.getSource();
+		int index = list.locationToIndex(evt.getPoint());
 		if (index<0) return;
 		Assignment selected = (Assignment) list.getSelectedValue();
-		
-		//TODO: do the rest if double click or make a GoToPage Button
-		//set the assignment, refresh the assignmentPage
-		ProfGUI pg = ((ProfGUI) controller.getFrame());
-		pg.getProfAssignmentPanel().setAssignment(selected);
-		controller.refreshProfAssignmentPage(pg);
-		pg.setActiveCard("PROFASSIGNMENTPAGE"); // go to the assignmentPage
-		
-	}
-}
 
+		// if Double-click detected
+		if (evt.getClickCount() == 2) {
+			//set the assignment, refresh the assignmentPage
+			ProfGUI pg = ((ProfGUI) controller.getFrame());
+			pg.getProfAssignmentPanel().setAssignment(selected);
+			controller.refreshProfAssignmentPage(pg);
+			pg.setActiveCard("PROFASSIGNMENTPAGE"); // go to the assignmentPage
+
+		} 
+	}
+	
+}
